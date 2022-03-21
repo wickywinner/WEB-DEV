cd033ed4d47a94e8767ff635d6759f89f9580665
wickywinner <winner312.com@gmail.com>
2022-03-21 10:51:12 +0530

    completed lec17

--- lec17/web_scraping/async.js
+++ lec17/web_scraping/async.js
@@ -1,5 +1,12 @@
 //async programming :- A process operates independently of other process
-
+`A-Start ------------------------------------------ A-End   
+| B-Start -----------------------------------------|--- B-End   
+|    |      C-Start ------------------- C-End      |      |   
+|    |       |                           |         |      |
+V    V       V                           V         V      V      
+1 thread->|<-A-|<--B---|<-C-|-A-|-C-|--A--|-B-|--C-->|---A---->|--B-->|
+` 
+// Above is an example of async programming that A,B and C all three operates independently of each others
 const fs=require("fs");
 console.log("before");
 fs.readFile("f1.txt",cb);
