2edb1b7110329b94832ed49e42937afd8455b1c9
wickywinner <winner312.com@gmail.com>
2022-03-22 21:16:10 +0530

    lec19 completed

--- lec19/crickinfo_scrapper/main.js
+++ lec19/crickinfo_scrapper/main.js
@@ -1,5 +1,6 @@
 const request = require("request");
 const cheerio = require("cheerio");
+const getAllMatchesLinkobj = require("./Allmatch");
 
 //step :-1
 const url = "https://www.espncricinfo.com/series/ipl-2020-21-1210595";
@@ -21,33 +22,9 @@ let anchorEle = $("a[data-hover='View All Results']");
 let link = anchorEle.attr("href");
 let fulllink = "https://www.espncricinfo.com"+link;
 //console.log(fulllink);
-getAllMatchesLink(fulllink);
+getAllMatchesLinkobj.gAllmatches(fulllink);
 }
 
-//step :-2
-function getAllMatchesLink(url)
-{
-    request(url,function (err,response,html)
-    {
-        if(err){
-            console.log(err);
-        }
-        else{
-            //console.log(html);
-            extractAllLink(html);
-        }
-    }
-    )
-}
 
-function extractAllLink(html)
-{
-    const $ = cheerio.load(html);
-    let scorecardele = $("a[data-hover='Scorecard']");
-    for(let j=0;j<scorecardele.length;j++){
-        let link = $(scorecardele[j]).attr("href");
-        let finallink = "https://www.espncricinfo.com"+link;
-        console.log(finallink);
-    }
-}
+
 
